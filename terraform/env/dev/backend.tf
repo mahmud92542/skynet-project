53e92cae748e60bd3d212d157c8385989abc7bf6
Mohammad Mahmud Hasan <34007876+mahmud92542@users.noreply.github.com>
2026-03-11 14:59:53 +0600

    Delete terraform/env/dev directory

--- terraform/env/dev/backend.tf
+++
@@ -1,6 +0,0 @@
-terraform {
-  backend "gcs" {
-    bucket = "skynet-terraform-state-mahmud" 
-    prefix = "terraform/state"
-  }
-}
--- terraform/env/dev/provider.tf
+++
@@ -1,8 +0,0 @@
-provider "google-beta" {
-  project = var.project_id
-  region  = var.region
-  default_labels = ({
-    environment = "dev"
-    owner       = "mahmud-sre-interviewee"
-  })
-}
